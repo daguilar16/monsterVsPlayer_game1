8b2a642a6da53d08c632bf33b78a700b6b07f923
daguilar16 <jaguilara@ucenfotec.ac.cr>
2019-12-26 16:24:01 -0600

    creacion de player1 y monstruo en diccionario

---
+++ game1.py
@@ -0,0 +1,17 @@
+#playerName = "Dave"
+#playerAttack = 10
+#playerHeal = 16
+#playerHealth = 100
+
+#LIST:
+#player = ['Dave', 10, 16, 100]
+
+#DICTIONARY: 
+player = {'name': 'Dave', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name':'Maximus', 'attack': 12, 'health': 100}
+
+print('Please select action')
+print('1. Attack')
+print('2. Heal')
+
+input()
