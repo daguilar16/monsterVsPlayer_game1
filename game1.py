84c4fd2e77217036249401e0764844a4e63ef244
daguilar16 <jaguilara@ucenfotec.ac.cr>
2019-12-26 17:02:57 -0600

    estructura basica del juego

--- game1.py
+++ game1.py
@@ -9,9 +9,27 @@
 #DICTIONARY: 
 player = {'name': 'Dave', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name':'Maximus', 'attack': 12, 'health': 100}
+gameRunning = True
 
-print('Please select action')
-print('1. Attack')
-print('2. Heal')
+while gameRunning == True:
 
-input()
+    print('Please select action')
+    print('1. Attack')
+    print('2. Heal')
+
+    playerChoise = input()
+
+    if playerChoise == str(1): 
+        monster['health'] = monster['health'] - player['attack']
+        player['health'] = player['health'] - monster['attack']
+        print (monster['health'])
+        print (player['health'])
+
+    elif playerChoise == str(2):
+        print('Heal player')
+    else: 
+        print('Invalid input')
+
+    if player['health'] <= 0:
+        gameRunning = False
+
